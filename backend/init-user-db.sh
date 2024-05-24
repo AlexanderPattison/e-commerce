df71228c5df7eb181a5e62cb9d36ce422bd053b4
Alexander Pattison <alex.pattison777@gmail.com>
2024-05-23 21:05:59 -0400

    rm docker and update files

--- backend/init-user-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-  CREATE USER user WITH ENCRYPTED PASSWORD 'password';
-  CREATE DATABASE mydatabase;
-  GRANT ALL PRIVILEGES ON DATABASE mydatabase TO user;
-EOSQL
