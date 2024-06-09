face21bb50e72083b8865f9a85d139e0f4b76234
Alexander Pattison <alex.pattison777@gmail.com>
2024-06-09 16:02:57 -0400

    Add backend and frontend directories

---
+++ frontend/src/app/homepage/AppBarClient.tsx
@@ -0,0 +1,76 @@
+﻿'use client';
+
+import React from 'react';
+import { AppBar, Toolbar, IconButton, Typography, InputBase } from '@mui/material';
+import { Search as SearchIcon, AccountCircle } from '@mui/icons-material';
+import { styled, alpha } from '@mui/material/styles';
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
+const AppBarClient: React.FC = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" noWrap>
+                    Home
+                </Typography>
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Search…"
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+                <div style={{ flexGrow: 1 }} />
+                <IconButton
+                    edge="end"
+                    aria-label="account of current user"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default AppBarClient;
---
+++ frontend/src/components/StoreProvider.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+import { Provider } from 'react-redux';
+import store from '../store';
+
+interface StoreProviderProps {
+    children: ReactNode;
+}
+
+const StoreProvider: React.FC<StoreProviderProps> = ({ children }) => (
+    <Provider store={store}>
+        {children}
+    </Provider>
+);
+
+export default StoreProvider;
