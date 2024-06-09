05ef949e4fbfee006c95e93444f54f107c6bedcf
Alexander Pattison <alex.pattison777@gmail.com>
2024-06-09 15:44:21 -0400

    backend and frontend folders with landing and homepage

--- nextjs/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"gKqNihV+jZ2+NzPD+yGUpD8S9Izb2dCHJzjxCazXYV4=\"\n}"
--- nextjs/src/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-// Import your reducers here
-
-const rootReducer = combineReducers({
-    // Add reducers here
-});
-
-export default rootReducer;
